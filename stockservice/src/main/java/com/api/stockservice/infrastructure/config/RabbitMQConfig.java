485e915e5bfcbb940b28135e1772432c6e0565ee
oussamahdidou <oussamahdidou15@gmail.com>
2024-09-30 00:33:33 +0100

    first commit

--- stockservice/src/main/java/com/api/stockservice/infrastructure/config/RabbitMQConfig.java
+++ stockservice/src/main/java/com/api/stockservice/infrastructure/config/RabbitMQConfig.java
@@ -1,8 +1,37 @@
 package com.api.stockservice.infrastructure.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
+    public static final String ARTICLE_QUEUE = "article_created_queue";
+    public static final String ARTICLE_EXCHANGE = "article_exchange";
+    public static final String ARTICLE_ROUTING_KEY = "article_created";
+
+    // Declare a queue
+    @Bean
+    public Queue articleQueue() {
+        return new Queue(ARTICLE_QUEUE, true); // durable queue
+    }
+
+    // Declare a direct exchange
+    @Bean
+    public DirectExchange articleExchange() {
+        return new DirectExchange(ARTICLE_EXCHANGE);
+    }
+
+    // Binding the queue and exchange with a routing key
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(articleQueue())
+                .to(articleExchange())
+                .with(ARTICLE_ROUTING_KEY);
+    }
 }
