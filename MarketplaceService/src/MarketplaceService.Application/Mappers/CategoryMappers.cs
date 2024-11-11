d42fdb21ebcd05084f3b3ea6081709b3379e5a3f
oussamahdidou <oussamahdidou15@gmail.com>
2024-11-11 02:24:11 +0100

    product service category service

---
+++ MarketplaceService/src/MarketplaceService.Application/Mappers/CategoryMappers.cs
@@ -0,0 +1,23 @@
+﻿using MarketplaceService.Application.Dtos.Category;
+using MarketplaceService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceService.Application.Mappers
+{
+    public static class CategoryMappers
+    {
+        public static CategorieItem FromCategorieToItem(this Category category)
+        {
+            return new CategorieItem()
+            {
+                CategoryId = category.CategoryId,
+                Name = category.Name,
+                Thumbnail = category.Thumbnail,
+            };
+        }
+    }
+}
---
+++ MarketplaceService/src/MarketplaceService.Application/Services/CategoryService.cs
@@ -0,0 +1,29 @@
+﻿using MarketplaceService.Application.Dtos.Category;
+using MarketplaceService.Application.Interfaces;
+using MarketplaceService.Application.Mappers;
+using MarketplaceService.Domain.Entities;
+using MarketplaceService.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceService.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategorieItem>> GetCategoriesAsync()
+        {
+           List<Category> categories = await categoryRepository.GetAllCategoriesAsync();
+            return categories.Select(x=>x.FromCategorieToItem()).ToList();
+        }
+    }
+}
