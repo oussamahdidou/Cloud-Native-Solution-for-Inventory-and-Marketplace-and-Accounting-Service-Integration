e6ab34f5eec8df239dcf8c061184496c8e8ac9d1
oussamahdidou <oussamahdidou15@gmail.com>
2024-12-05 23:51:06 +0100

    hosting

--- client/adminpanel/free-react-tailwind-admin-dashboard/src/components/Charts/ChartOne.tsx
+++ client/adminpanel/free-react-tailwind-admin-dashboard/src/components/Charts/ChartOne.tsx
@@ -126,12 +126,12 @@ const ChartOne: React.FC = () => {
   const [state, setState] = useState<ChartOneState>({
     series: [
       {
-        name: 'Product One',
+        name: 'Revenus',
         data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
       },
 
       {
-        name: 'Product Two',
+        name: 'Sales',
         data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
       },
     ],
@@ -167,19 +167,7 @@ const ChartOne: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="flex w-full max-w-45 justify-end">
-          <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
-            <button className="rounded bg-white py-1 px-3 text-xs font-medium text-black shadow-card hover:bg-white hover:shadow-card dark:bg-boxdark dark:text-white dark:hover:bg-boxdark">
-              Day
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
-              Week
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
-              Month
-            </button>
-          </div>
-        </div>
+        <div className="flex w-full max-w-45 justify-end"></div>
       </div>
 
       <div>
