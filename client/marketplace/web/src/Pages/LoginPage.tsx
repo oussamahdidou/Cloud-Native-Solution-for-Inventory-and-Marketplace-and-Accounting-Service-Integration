6596d73232e33acf26647a991f9d707883e5b556
oussamahdidou <oussamahdidou15@gmail.com>
2024-12-09 01:28:29 +0100

    toast handler

--- client/marketplace/web/src/Pages/LoginPage.tsx
+++ client/marketplace/web/src/Pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import { useAuth } from "../Contexts/useAuth";
 import { useForm } from "react-hook-form";
+import toast, { Toaster } from "react-hot-toast";
 type Props = {};
 type LoginFormInputs = {
   username: string;
@@ -21,121 +22,130 @@ const LoginPage = (props: Props) => {
     formState: error,
   } = useForm<LoginFormInputs>({ resolver: yupResolver(validation) });
   const handleLogin = (form: LoginFormInputs) => {
-    login(form.username, form.password);
+    toast.loading("Waiting...");
+    try {
+      login(form.username, form.password);
+    } catch (error) {
+      toast.success("something went wrong");
+    }
   };
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="mx-auto max-w-screen-xl  px-4 py-16 sm:px-6 lg:px-8 shadow-lg">
-        <div className="mx-auto max-w-lg text-center">
-          <h1 className="text-2xl font-bold sm:text-3xl">Welcome Back!</h1>
-          <p className="mt-4 text-gray-600">
-            login to our store so u can add items to your cart and buy products
-          </p>
-        </div>
-
-        <form
-          className="mx-auto mb-0 mt-8 max-w-md space-y-4"
-          onSubmit={handleSubmit(handleLogin)}
-        >
-          <div>
-            <label className="sr-only" htmlFor="email">
-              Email
-            </label>
-            <div className="relative">
-              <input
-                placeholder="Enter your email"
-                className="w-full rounded-lg border-gray-300 p-4 pe-12 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
-                id="email"
-                type="text"
-                {...register("username")}
-              />
-              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                <svg
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  className="h-6 w-6 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                    stroke-width="2"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-            {error.errors.username ? (
-              <span className="text-red-600">
-                {error.errors.username?.message}
-              </span>
-            ) : (
-              <></>
-            )}
-          </div>
-
-          <div>
-            <label className="sr-only" htmlFor="password">
-              Password
-            </label>
-            <div className="relative">
-              <input
-                placeholder="Enter your password"
-                className="w-full rounded-lg border-gray-300 p-4 pe-12 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
-                id="password"
-                type="password"
-                {...register("password")}
-              />
-              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                <svg
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  className="h-6 w-6 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    stroke-width="2"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                  ></path>
-                  <path
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                    stroke-width="2"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-            {error.errors.password ? (
-              <span className="text-red-600">
-                {error.errors.password?.message}
-              </span>
-            ) : (
-              <></>
-            )}
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="text-sm text-gray-600">
-              No account yet?
-              <Link to={`/register`}>
-                <span className="underline">Create one</span>
-              </Link>
+    <>
+      <Toaster></Toaster>
+      <div className="flex items-center justify-center h-screen">
+        <div className="mx-auto max-w-screen-xl  px-4 py-16 sm:px-6 lg:px-8 shadow-lg">
+          <div className="mx-auto max-w-lg text-center">
+            <h1 className="text-2xl font-bold sm:text-3xl">Welcome Back!</h1>
+            <p className="mt-4 text-gray-600">
+              login to our store so u can add items to your cart and buy
+              products
             </p>
-            <button
-              className="inline-block rounded-lg bg-purple-600 px-5 py-3 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-              type="submit"
-            >
-              Sign In
-            </button>
           </div>
-        </form>
+
+          <form
+            className="mx-auto mb-0 mt-8 max-w-md space-y-4"
+            onSubmit={handleSubmit(handleLogin)}
+          >
+            <div>
+              <label className="sr-only" htmlFor="email">
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  placeholder="Enter your email"
+                  className="w-full rounded-lg border-gray-300 p-4 pe-12 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
+                  id="email"
+                  type="text"
+                  {...register("username")}
+                />
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="h-6 w-6 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                    ></path>
+                  </svg>
+                </span>
+              </div>
+              {error.errors.username ? (
+                <span className="text-red-600">
+                  {error.errors.username?.message}
+                </span>
+              ) : (
+                <></>
+              )}
+            </div>
+
+            <div>
+              <label className="sr-only" htmlFor="password">
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  placeholder="Enter your password"
+                  className="w-full rounded-lg border-gray-300 p-4 pe-12 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
+                  id="password"
+                  type="password"
+                  {...register("password")}
+                />
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="h-6 w-6 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                    ></path>
+                    <path
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                    ></path>
+                  </svg>
+                </span>
+              </div>
+              {error.errors.password ? (
+                <span className="text-red-600">
+                  {error.errors.password?.message}
+                </span>
+              ) : (
+                <></>
+              )}
+            </div>
+
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-600">
+                No account yet?
+                <Link to={`/register`}>
+                  <span className="underline">Create one</span>
+                </Link>
+              </p>
+              <button
+                className="inline-block rounded-lg bg-purple-600 px-5 py-3 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- client/marketplace/web/src/Pages/RegisterPage.tsx
+++ client/marketplace/web/src/Pages/RegisterPage.tsx
@@ -4,6 +4,7 @@ import * as Yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useAuth } from "../Contexts/useAuth";
+import toast, { Toaster } from "react-hot-toast";
 type Props = {};
 type RegisterFormInputs = {
   email: string;
@@ -27,121 +28,126 @@ const RegisterPage = (props: Props) => {
     formState: error,
   } = useForm<RegisterFormInputs>({ resolver: yupResolver(validation) });
   const handleRegister = (form: RegisterFormInputs) => {
+    toast.loading("Waiting...");
+
     RegisterUser(form.email, form.username, form.password);
   };
   return (
-    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form onSubmit={handleSubmit(handleRegister)}>
-            <div>
-              <label
-                className="block text-sm font-medium text-gray-700"
-                htmlFor="username"
-              >
-                Username
-              </label>
-              <div className="mt-1">
-                <input
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  type="text"
-                  id="username"
-                  {...register("username")}
-                />
-                {error.errors.username ? (
-                  <span className="text-red-600">
-                    {error.errors.username?.message}
-                  </span>
-                ) : (
-                  <></>
-                )}
+    <>
+      <Toaster></Toaster>
+      <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form onSubmit={handleSubmit(handleRegister)}>
+              <div>
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="username"
+                >
+                  Username
+                </label>
+                <div className="mt-1">
+                  <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    type="text"
+                    id="username"
+                    {...register("username")}
+                  />
+                  {error.errors.username ? (
+                    <span className="text-red-600">
+                      {error.errors.username?.message}
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
 
-            <div className="mt-6">
-              <label
-                className="block text-sm font-medium text-gray-700"
-                htmlFor="email"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  type="text"
-                  id="email"
-                  {...register("email")}
-                />
-                {error.errors.email ? (
-                  <span className="text-red-600">
-                    {error.errors.email?.message}
-                  </span>
-                ) : (
-                  <></>
-                )}
+              <div className="mt-6">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="email"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    type="text"
+                    id="email"
+                    {...register("email")}
+                  />
+                  {error.errors.email ? (
+                    <span className="text-red-600">
+                      {error.errors.email?.message}
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
 
-            <div className="mt-6">
-              <label
-                className="block text-sm font-medium text-gray-700"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  type="password"
-                  id="password"
-                  {...register("password")}
-                />
-                {error.errors.password ? (
-                  <span className="text-red-600">
-                    {error.errors.password?.message}
-                  </span>
-                ) : (
-                  <></>
-                )}
+              <div className="mt-6">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    type="password"
+                    id="password"
+                    {...register("password")}
+                  />
+                  {error.errors.password ? (
+                    <span className="text-red-600">
+                      {error.errors.password?.message}
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
 
-            <div className="mt-6">
-              <label
-                className="block text-sm font-medium text-gray-700"
-                htmlFor="confirm-password"
-              >
-                Confirm Password
-              </label>
-              <div className="mt-1">
-                <input
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  type="password"
-                  id="confirm-password"
-                  {...register("confirmPassword")}
-                />
-                {error.errors.confirmPassword ? (
-                  <span className="text-red-600">
-                    {error.errors.confirmPassword?.message}
-                  </span>
-                ) : (
-                  <></>
-                )}
+              <div className="mt-6">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="confirm-password"
+                >
+                  Confirm Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    type="password"
+                    id="confirm-password"
+                    {...register("confirmPassword")}
+                  />
+                  {error.errors.confirmPassword ? (
+                    <span className="text-red-600">
+                      {error.errors.confirmPassword?.message}
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
 
-            <div className="mt-6">
-              <button
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                type="submit"
-              >
-                Sign up
-              </button>
-            </div>
-          </form>
+              <div className="mt-6">
+                <button
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  type="submit"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
