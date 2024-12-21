8d916ca3d44467fa7fcc0e095017a47766616bbd
oussamahdidou <oussamahdidou15@gmail.com>
2024-12-21 01:50:18 +0100

    add xunit test library

---
+++ backend/services/MarketplaceService/test/MarketplaceService.API.tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using MarketplaceService.API.Controllers;
+using MarketplaceService.Application.Dtos.Products;
+using MarketplaceService.Application.Interfaces;
+using MarketplaceService.Domain.Queries;
+using Microsoft.AspNetCore.Mvc;
+namespace MarketplaceService.API.tests.Controllers
+{
+
+    public class ProductControllerTests
+    {
+        private readonly IProductService productService;
+        private readonly ProductController productController;
+        public ProductControllerTests()
+        {
+            productService = A.Fake<IProductService>();
+            productController = new ProductController(productService);
+        }
+        [Fact]
+        public async Task ProductController_GetProducts_ReturnOkAsync()
+        {
+            //Arrange
+            var fakeQuery = A.Fake<ProductQuery>();
+            var fakeProducts = A.Fake<List<ProductItem>>();
+            A.CallTo(() => productService.GetProductItems(fakeQuery)).Returns(Task.FromResult(fakeProducts));
+            // Act
+            var result = await productController.GetProducts(fakeQuery);
+            // Assert
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeEquivalentTo(fakeProducts);
+            // Verify the method was called with the correct query
+            A.CallTo(() => productService.GetProductItems(fakeQuery)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
